f84b14cf077a53764e0b23cb83172061917ce8b0
Garhan Attebury <garhan.attebury@unl.edu>
2015-02-28 20:19:31 -0600

    did I have a local puppet to run the validate parser against before? no ... do I now? yes

--- manifests/params.pp
+++ manifests/params.pp
@@ -6,8 +6,8 @@ class glexec::params {
     $lcmaps_db_file             = '/etc/lcmaps.db'
     $lcmaps_debug_level         = '3'
     $lcmaps_get_account_policy  = 'glexec'
-    $lcmaps_log_file            = '/var/log/glexec/lcas_lcmaps.log',
-    $log_destination            = 'syslog',
+    $lcmaps_log_file            = '/var/log/glexec/lcas_lcmaps.log'
+    $log_destination            = 'syslog'
     $log_level                  = '3'
     $log_file                   = '/var/log/glexec/glexec_log'
     $syslog_facility            = 'LOG_DAEMON'
